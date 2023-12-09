e9c5fdc177e4cf7ecbcd77889c65820a228e33f8
joanjajas <joanbeltranperis@gmail.com>
2023-12-09 13:58:36 +0100

    (2023) day9

---
+++ aoc_2023/src/bin/day9.rs
@@ -0,0 +1,90 @@
+use std::fs;
+
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let input = fs::read_to_string("input/day9.txt")?;
+
+    let part1 = part_1(&input);
+    let part2 = part_2(&input);
+
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
+
+    Ok(())
+}
+
+fn part_1(input: &str) -> i64 {
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let history = generate_history(line);
+
+        sum += history
+            .iter()
+            .rev()
+            .skip(1)
+            .fold(0, |acc, x| acc + x.last().unwrap());
+    }
+
+    sum
+}
+
+fn part_2(input: &str) -> i64 {
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let history = generate_history(line);
+
+        sum += history
+            .iter()
+            .rev()
+            .skip(1)
+            .fold(0, |acc, x| x.first().unwrap() - acc);
+    }
+
+    sum
+}
+
+fn generate_history(input: &str) -> Vec<Vec<i64>> {
+    let mut history: Vec<Vec<i64>> = Vec::new();
+
+    let mut history_step: Vec<i64> = input
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    history.push(history_step.clone());
+
+    while !history_step.iter().all(|x| x == &0) {
+        history_step = history_step.windows(2).map(|w| w[1] - w[0]).collect();
+        history.push(history_step.clone());
+    }
+
+    history
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() -> Result<()> {
+        let input = fs::read_to_string("input/test/day9.txt")?;
+
+        let result = part_1(&input);
+        assert_eq!(result, 114);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> Result<()> {
+        let input = fs::read_to_string("input/test/day9.txt")?;
+
+        let result = part_2(&input);
+        assert_eq!(result, 6);
+
+        Ok(())
+    }
+}
